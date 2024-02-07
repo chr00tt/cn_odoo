7cbbdc436cb5164e1f5b0ba77c75e866936088e8
He Jian <hej@eabax.com>
2024-02-07 21:49:37 +0800

    fix

---
+++ cn_account/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'cn Invoicing',
+    'version' : '1.2',
+    'summary': 'Invoices & Payments',
+    'category': 'Accounting/Accounting',
+    'depends': ['account'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
