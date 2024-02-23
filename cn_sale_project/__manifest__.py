4407b5f19c513f01f801e29acd9728220e14dbe2
He Jian <hej@eabax.com>
2024-02-23 17:08:37 +0800

    fix

---
+++ cn_sale_project/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "cn Sales - Project",
+    'category': 'Hidden',
+    'depends': ['sale_project'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
