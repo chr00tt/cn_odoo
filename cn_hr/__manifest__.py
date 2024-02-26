95e0532977149b97366d76a13be9eadaeff052e8
He Jian <hej@eabax.com>
2024-02-26 18:43:56 +0800

    fix

---
+++ cn_hr/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Employees',
+    'version': '1.1',
+    'author': "He Jian",
+    'category': 'Human Resources/Employees',
+    'depends': [
+        'hr',
+    ],
+    'data': [
+        'data/hr_data.xml',
+    ],
+    'demo': [
+        'data/hr_demo.xml'
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
