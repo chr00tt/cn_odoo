f3bce6f10b0a47466e84618fe7b6c1ce98246e94
He Jian <hej@eabax.com>
2024-02-27 20:36:27 +0800

    fix

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Chinese Odoo
 
 主要是修正翻译。
+
+由于需要覆盖翻译，需要在odoo.conf里添加以下设置：
+overwrite_existing_translations = True
---
+++ cn_crm/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn CRM',
+    'author': 'He Jian',
+    'category': 'Sales/CRM',
+    'depends': [
+        'crm',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
