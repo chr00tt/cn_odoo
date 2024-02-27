404193c1ce03f9e9c466b2ed31f48dd2727abd14
He Jian <hej@eabax.com>
2024-02-27 22:09:14 +0800

    fix

---
+++ cn_base_import_module/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'cn Base import module',
+    'author': 'He Jian',
+    'category': 'Hidden/Tools',
+    'depends': ['base_import_module'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
