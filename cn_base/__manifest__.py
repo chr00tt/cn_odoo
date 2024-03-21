8a53b1a1fbec5f2aa1ef49c92c02ae38ca442c6b
He Jian <hej@eabax.com>
2024-03-21 17:12:35 +0800

    fix

--- cn_base/__manifest__.py
+++ cn_base/__manifest__.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
 {
-    'name' : 'cn Invoicing',
+    'name': 'cn Base',
     'author': 'He Jian',
-    'category': 'Accounting/Accounting',
-    'depends': ['account'],
+    'category': 'Hidden',
+    'demo': [
+        'data/res_users_demo.xml',
+    ],
+    'depends': ['base'],
     'installable': True,
     'auto_install': True,
     'license': 'LGPL-3',
