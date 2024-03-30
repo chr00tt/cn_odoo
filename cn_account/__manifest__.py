1124deb6cb46999b29ba6827614d56f95aa24c2c
He Jian <hej@eabax.com>
2024-03-30 21:46:07 +0800

    fix

---
+++ cn_account/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'cn Invoicing',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    'category': 'Accounting/Accounting',
+    'depends' : ['account'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ cn_mrp/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn Manufacturing',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    'category': 'Manufacturing/Manufacturing',
+    'depends': ['mrp'],
+    'demo': [
+        'data/mrp_demo.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
