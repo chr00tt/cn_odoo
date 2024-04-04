ba7b5b83ec83142a37facd8078535effc54f88fc
He Jian <hej@eabax.com>
2024-04-04 18:27:05 +0800

    fix

---
+++ cn_account_budget/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Budgets Management',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    'category': 'Accounting',
+    'depends': ['account_budget'],
+    'installable': True,
+    'auto_install': True,
+}
