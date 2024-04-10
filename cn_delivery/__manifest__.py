b95c11ebe780ded2ba44862275236a22d72a7e8d
He Jian <hej@eabax.com>
2024-04-10 23:19:08 +0800

    fix

---
+++ cn_delivery/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn Delivery Costs',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    'category': 'Stock',
+    'depends': ['delivery'],
+    'installable': True,
+    'auto_install': True,
+}
