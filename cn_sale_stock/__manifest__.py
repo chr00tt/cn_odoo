42f30038cf20dc03539e636b52ce33a24b103bef
He Jian <hej@eabax.com>
2024-04-11 10:08:17 +0800

    fix

---
+++ cn_sale_stock/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Sales and Warehouse Management',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    'category': 'Hidden',
+    'depends': ['sale_stock'],
+    'installable': True,
+    'auto_install': True,
+}
