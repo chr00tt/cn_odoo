2c04b55342d92a00cf151bdf38407d58a1272d56
He Jian <hej@eabax.com>
2024-04-11 09:36:15 +0800

    fix

---
+++ cn_stock/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Inventory Management',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    'depends': ['stock'],
+    'category': 'Warehouse',
+    'installable': True,
+    'auto_install': True,
+}
