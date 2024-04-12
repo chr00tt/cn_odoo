62d0ffad12a0e6944486fffc7d25593b42fed18d
He Jian <hej@eabax.com>
2024-04-12 09:25:01 +0800

    fix

---
+++ cn_account_asset_management/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "cn Assets Management",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    "category": "Accounting & Finance",
+    "depends": ["account_asset_management"],
+    "demo": [
+        'data/account_asset_group_demo.xml',
+        'data/account_asset_profile_demo.xml',
+    ],
+    'auto_install': True,
+    "license": "AGPL-3",
+}
