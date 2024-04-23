9fa1c6744e2fbaf5db3bd17ba977d815808f06e6
He Jian <hej@eabax.com>
2024-04-23 21:13:50 +0800

    fix

---
+++ cn_hr_expense/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn Expenses',
+    'author': "He Jian",
+    'category': 'Human Resources/Expenses',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['hr_expense'],
+    'demo': ['data/hr_expense_demo.xml'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
