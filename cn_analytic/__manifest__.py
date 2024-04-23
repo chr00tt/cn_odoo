edc1d2a5b103a948b4ed7532de0c4368d6dd4e36
He Jian <hej@eabax.com>
2024-04-23 11:47:18 +0800

    fix

---
+++ cn_analytic/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name' : 'cn Analytic Accounting',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    'category': 'Accounting/Accounting',
+    'depends' : ['analytic'],
+    'demo': [
+        'data/analytic_account_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
