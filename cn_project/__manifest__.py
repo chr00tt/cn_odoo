df1affc66f94dadaa4d7808befe1c7105de0cac0
He Jian <hej@eabax.com>
2024-04-23 18:54:20 +0800

    fix

--- cn_project/__manifest__.py
+++ cn_project/__manifest__.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
-    'name' : 'cn Invoicing',
-    'author': "He Jian",
-    'category': 'Accounting/Accounting',
+    'name': 'cn Project',
     'website': 'https://github.com/chr00tt/cn_odoo',
-    'depends' : ['account'],
+    'author': "He Jian",
+    'category': 'Services/Project',
+    'depends': [
+        'project',
+    ],
+    'demo': ['data/project_demo.xml'],
     'installable': True,
     'auto_install': True,
     'license': 'LGPL-3',
