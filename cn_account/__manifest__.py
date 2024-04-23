1aa5edf9f17a8bbaa32ee59278289d9446ad6167
He Jian <hej@eabax.com>
2024-04-23 20:51:29 +0800

    fix

--- cn_account/__manifest__.py
+++ cn_account/__manifest__.py
@@ -6,6 +6,15 @@
     'category': 'Accounting/Accounting',
     'website': 'https://github.com/chr00tt/cn_odoo',
     'depends' : ['account'],
+    'data': [
+        'data/cn_account.xml',
+        'data/account.account.template.csv',
+        'data/cn_account_post.xml',
+    ],
+    'demo': [
+        'data/cn_account_demo.xml',
+        'data/account_account_demo.xml',
+    ],
     'installable': True,
     'auto_install': True,
     'license': 'LGPL-3',
