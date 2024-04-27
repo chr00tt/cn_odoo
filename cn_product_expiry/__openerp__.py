12774fdf369516b79f23610f6746143e5211fe54
He Jian <hej@eabax.com>
2024-04-27 09:44:06 +0800

    fix

--- cn_product_expiry/__openerp__.py
+++ cn_product_expiry/__openerp__.py
@@ -1,9 +1,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name' : 'cn Products Expiration Date',
-    'website': 'https://github.com/chr00tt/cn_odoo',
     'author': "He Jian",
     'category' : 'Warehouse',
+    'website': 'https://github.com/chr00tt/cn_odoo',
     'depends' : ['product_expiry'],
     'auto_install': True,
     'installable': True,
---
+++ cn_purchase/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn Purchase Management',
+    'author': "He Jian",
+    'category': 'Purchases',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['purchase'],
+    'demo': [
+        'data/cn_purchase_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
