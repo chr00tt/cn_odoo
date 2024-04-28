f8621bad9b6ad3e0fe3fcf88e228783998f56790
He Jian <hej@eabax.com>
2024-04-29 00:58:20 +0800

    fix

---
+++ cn_sale/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn Sales',
+    'author': "He Jian",
+    'category': 'Sales/Sales',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': [
+        'sale',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
