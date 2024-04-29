025fb8dd065769f728302cf9a80a03e6f36012c1
He Jian <hej@eabax.com>
2024-04-29 20:58:51 +0800

    fix

--- cn_account/__manifest__.py
+++ cn_account/__manifest__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
 {
-    'name': 'cn Expense Tracker',
+    'name' : 'cn Invoicing',
     'author': "He Jian",
-    'category': 'Human Resources',
+    'category': 'Accounting',
     'website': 'https://github.com/chr00tt/cn_odoo',
-    'depends': ['hr_expense'],
+    'depends' : ['account'],
+    'demo': [
+        'data/cn_account_demo.xml',
+    ],
     'installable': True,
+    'application': False,
     'auto_install': True,
 }
