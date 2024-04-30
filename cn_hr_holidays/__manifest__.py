ddb9324d3f89bae83263b1a9b4980e0cf154490b
He Jian <hej@eabax.com>
2024-04-30 16:52:18 +0800

    [i18n] hr_holidays

---
+++ cn_hr_holidays/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Leave Management',
+    'category': 'Human Resources',
+    'depends': ['hr_holidays'],
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
