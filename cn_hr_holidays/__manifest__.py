8d59a60ee3cc43836b7580198bb5d74fcca408ab
He Jian <hej@eabax.com>
2024-04-30 08:26:08 +0800

    fix

--- cn_hr_holidays/__manifest__.py
+++ cn_hr_holidays/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 {
-    'name': 'cn Expenses',
+    'name': 'cn Time Off',
     'author': "He Jian",
-    'category': 'Human Resources/Expenses',
+    'category': 'Human Resources/Time Off',
     'website': 'https://github.com/chr00tt/cn_odoo',
-    'depends': ['hr_expense'],
-    'demo': ['data/hr_expense_demo.xml'],
+    'depends': ['hr_holidays'],
     'installable': True,
+    'application': False,
     'auto_install': True,
     'license': 'LGPL-3',
 }
