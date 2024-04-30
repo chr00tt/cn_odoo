ee2fe156811199320e4c9ad63b8d305bc87714c0
He Jian <hej@eabax.com>
2024-04-30 17:25:35 +0800

    fix

---
+++ cn_base/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn Base',
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['base'],
+    'data': [
+        'base_data.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
