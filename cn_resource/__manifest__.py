7c1b8ce4e0f1725aa73bcccfea75c4570ac90c93
He Jian <hej@eabax.com>
2024-04-30 08:33:26 +0800

    fix

---
+++ cn_resource/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Resource',
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['resource'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
