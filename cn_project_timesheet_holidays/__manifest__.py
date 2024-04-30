6807cc352f2973ef523085165a45f86e73499370
He Jian <hej@eabax.com>
2024-04-30 17:01:15 +0800

    [i18n] project_timesheet_holidays

--- cn_project_timesheet_holidays/__manifest__.py
+++ cn_project_timesheet_holidays/__manifest__.py
@@ -2,12 +2,11 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'cn Leave Management',
+    'name': 'cn Timesheet when on Leaves',
     'author': "He Jian",
     'category': 'Human Resources',
     'website': 'https://github.com/chr00tt/cn_odoo',
-    'depends': ['hr_holidays'],
+    'depends': ['project_timesheet_holidays'],
     'installable': True,
-    'application': False,
     'auto_install': True,
 }
