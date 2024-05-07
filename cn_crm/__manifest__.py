543ebf6c994da08503732513ca527560a72ca037
He Jian <hej@eabax.com>
2024-05-07 17:27:57 +0800

    [i18n] crm

---
+++ cn_crm/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn CRM',
+    'author': "He Jian",
+    'category': 'Sales/CRM',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': [
+        'crm',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
