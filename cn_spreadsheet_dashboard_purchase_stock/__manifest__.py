5dbf99d51107e64e6093bd443573ddf2d3c8d448
He Jian <hej@eabax.com>
2024-05-10 21:30:41 +0800

    [i18n] spreadsheet_dashboard

---
+++ cn_spreadsheet_dashboard_purchase_stock/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "cn Spreadsheet dashboard for purchases",
+    'category': 'Hidden',
+    'depends': ['spreadsheet_dashboard_purchase_stock'],
+    'data': [
+        "data/dashboards.xml",
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ cn_spreadsheet_dashboard_stock_account/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "cn Spreadsheet dashboard for stock",
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['spreadsheet_dashboard_stock_account'],
+    'data': [
+        "data/dashboards.xml",
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
