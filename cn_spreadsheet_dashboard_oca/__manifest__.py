5a84891f6aafa65dbd28d49c80bc5a2d4d0c30c7
He Jian <hej@eabax.com>
2024-05-10 10:47:50 +0800

    [i18n] spreadsheet

---
+++ cn_spreadsheet_dashboard_oca/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 CreuBlanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "cn Spreadsheet Dashboard Oca",
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": [
+        "spreadsheet_dashboard_oca",
+    ],
+    "auto_install": True,
+    "license": "AGPL-3",
+}
---
+++ cn_spreadsheet_oca/__manifest__.py
@@ -0,0 +1,11 @@
+# Copyright 2022 CreuBlanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "cn Spreadsheet Oca",
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["spreadsheet_oca"],
+    "auto_install": True,
+    "license": "AGPL-3",
+}
