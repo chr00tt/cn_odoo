b50f902a19a52548a3d5e355f06fb381e3f60e27
He Jian <hej@eabax.com>
2024-05-12 23:35:37 +0800

    [i18n] bi_sql_editor

---
+++ cn_bi_sql_editor/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "cn BI SQL Editor",
+    'author': "He Jian",
+    "category": "Reporting",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": [
+        "bi_sql_editor",
+    ],
+    "demo": ["demo/bi_sql_view_demo.xml"],
+    "installable": True,
+    "auto_install": True,
+    'license': 'LGPL-3',
+}
