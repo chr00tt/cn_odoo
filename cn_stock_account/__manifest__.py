3a0e3169d2671ba75f7760de38d73f081a2ea705
He Jian <hej@eabax.com>
2024-05-13 09:43:34 +0800

    [IMP] stock_account: demo

---
+++ cn_stock_account/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn WMS Accounting',
+    'depends': ['stock_account'],
+    'category': 'Hidden',
+    'demo': [
+        'data/cn_stock_account_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
