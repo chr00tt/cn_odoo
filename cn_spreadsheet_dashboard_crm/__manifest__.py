d014d229f2f7ad6120b69346a1cbed691ab49d11
He Jian <hej@eabax.com>
2024-05-13 16:41:39 +0800

    [i18n] spreadsheet_dashboard_crm

---
+++ cn_spreadsheet_dashboard_crm/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "cn Spreadsheet dashboard for CRM",
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['spreadsheet_dashboard_crm'],
+    'data': [
+        "data/dashboards.xml",
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
