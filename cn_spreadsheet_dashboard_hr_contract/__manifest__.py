4b8d3bc47f03cf3cfc8a85c021d6a69178638664
He Jian <hej@eabax.com>
2024-05-13 16:52:04 +0800

    [i18n] spreadsheet_dashboard_hr_contract

---
+++ cn_spreadsheet_dashboard_hr_contract/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "cn Spreadsheet dashboard for human resources",
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['spreadsheet_dashboard_hr_contract'],
+    'data': [
+        "data/dashboards.xml",
+    ],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
