c3cd6eec1705da2e20a170298de5f86a159ac73b
He Jian <hej@eabax.com>
2024-08-05 13:21:38 +0800

    [i18n] mail_bot

---
+++ cn_mail_bot/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'cn Bot',
+    'author': "He Jian",
+    'category': 'Productivity/Discuss',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['mail_bot'],
+    'installable': True,
+    'auto_install': True,
+    'application': False,
+    'license': 'AGPL-3',
+}
