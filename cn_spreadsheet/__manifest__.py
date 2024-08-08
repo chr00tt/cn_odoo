1f98ade1043d1c49d9c35a732ea9a075e9dff149
He Jian <hej@eabax.com>
2024-08-08 14:25:28 +0800

    [I18N] spreadsheet.

---
+++ cn_spreadsheet/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': "cn Spreadsheet",
+    'category': 'Hidden',
+    'depends': ['spreadsheet'],
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
