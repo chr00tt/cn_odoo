17b548ba31b3f624dde9db9ac80a78aa5507a219
He Jian <hej@eabax.com>
2024-08-30 13:39:11 +0800

    [I18N] report_py3o.

---
+++ cn_report_py3o/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2013 XCG Consulting (http://odoo.consulting)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "cn Py3o Report Engine",
+    'author': "He Jian",
+    "category": "Reporting",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["report_py3o"],
+    "installable": True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
