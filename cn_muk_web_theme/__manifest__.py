631fda542f513164c6fd14a00e75d31ab817ebe4
He Jian <hej@eabax.com>
2024-09-07 17:11:43 +0800

    fix

---
+++ cn_muk_web_theme/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn MuK Backend Theme',
+    'category': 'Themes/Backend',
+    'license': 'LGPL-3', 
+    'depends': [
+        'muk_web_theme',
+    ],
+    'auto_install': True,
+}
