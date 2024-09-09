78fdd1c1588871458b1f10eddc3b289e2ffcf0ea
He Jian <hej@eabax.com>
2024-09-09 23:09:58 +0800

    [ADD] account: 科目组.

--- cn_account/__manifest__.py
+++ cn_account/__manifest__.py
@@ -15,6 +15,7 @@
         'data/enterprise/enterprise_account_post.xml',
         'data/administrative_institution/administrative_institution_account.xml',
         'data/administrative_institution/account.account.template.csv',
+        'data/administrative_institution/account.group.template.csv',
         'data/administrative_institution/administrative_institution_account_post.xml',
     ],
     'demo': [
