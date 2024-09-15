f71be963477fc105c2a529c27c9288c9848decde
He Jian <hej@eabax.com>
2024-09-15 20:08:33 +0800

    [I18N] account_asset.

---
+++ cn_account_asset/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Assets Management',
+    'author': "He Jian",
+    'category': 'Accounting/Accounting',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['account_asset'],
+    'auto_install': True,
+    "license": "AGPL-3",
+}
