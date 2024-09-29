9ee661c8028f206a7bbf36023a055acc7a7509cd
He Jian <hej@eabax.com>
2024-09-29 21:17:17 +0800

    [I18N] account_accountant.

---
+++ cn_account_accountant/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'cn Accounting',
+    'author': "He Jian",
+    'category': 'Accounting/Accounting',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['account_accountant'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
