b2cf0c490f5525ab3cb7817d4fc1bf33acdd94e9
He Jian <hej@eabax.com>
2024-10-13 16:57:32 +0800

    fix

---
+++ cn_web_editor/__openerp__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'cn Web Editor',
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['web_editor'],
+    'auto_install': True
+}
