aef96be2aeaee98c9c6cae1fc6c57fa8b191021f
He Jian <hej@eabax.com>
2024-10-14 11:22:32 +0800

    [I18N] account_reports.

---
+++ cn_account_reports/__manifest__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'cn Accounting Reports',
+    'category': 'Accounting',
+    'depends': ['account_reports'],
+    'auto_install': True,
+}
