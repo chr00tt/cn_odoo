e0d28e83462958d62b4116e36bc7b90b3f02da3f
He Jian <hej@eabax.com>
2024-10-18 11:46:25 +0800

    fix

--- cn_account/models/chart_template.py
+++ cn_account/models/chart_template.py
@@ -5,10 +5,9 @@ from odoo import api, fields, models, Command, _, osv
 class AccountChartTemplate(models.Model):
     _inherit= "account.chart.template"
 
-    @api.model
-    def generate_journals(self, acc_template_ref, company, journals_dict=None):
-        result = super(AccountChartTemplate, self).generate_journals(acc_template_ref, company, journals_dict=journals_dict)
-        journal = self.env['account.journal'].search([('code', '=', _('MISC'))])
-        journal.write({'name': '手工录入', 'code': '记'})
-
-        return result
+    def _prepare_all_journals(self, acc_template_ref, company, journals_dict=None):
+        journal_data = super()._prepare_all_journals(acc_template_ref, company, journals_dict=journals_dict)
+        for journal in journal_data:
+            if journal['code'] == '杂项':
+                journal.update({'name': '手工录入', 'code': '记'})
+        return journal_data
