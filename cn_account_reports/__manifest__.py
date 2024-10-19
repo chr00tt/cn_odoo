b6b9b5c2271a80adee1074d8714ae41278364e29
He Jian <hej@eabax.com>
2024-10-19 20:33:13 +0800

    fix

---
+++ cn_account_reports/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'cnAccounting Reports',
+    'category': 'Accounting/Accounting',
+    'depends': ['account_reports'],
+    'auto_install': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
