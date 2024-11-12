3a3afdad56c783c10020db34da1ee840e194bae2
He Jian <hej@eabax.com>
2024-11-12 12:11:03 +0800

    [I18N] board.

---
+++ cn_board/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Dashboards',
+    'author': "He Jian",
+    'category': 'Productivity',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['board'],
+    'data': [
+        'views/board_views.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
