251c225848a5174454af1a9be96b4ea3cedd9dd2
He Jian <hej@eabax.com>
2024-12-04 08:59:06 +0800

    fix

---
+++ cn_account_asset/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Assets Management',
+    'depends': ['account_asset'],
+    'category': 'Accounting',
+    'auto_install': True,
+}
