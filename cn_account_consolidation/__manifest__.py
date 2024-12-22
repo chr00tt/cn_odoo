6396216fbf791e5abc2acb222afbaf73c1519aac
He Jian <hej@eabax.com>
2024-12-23 00:11:54 +0800

    fix

---
+++ cn_account_consolidation/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cn Consolidation",
+    'author': "He Jian",
+    'category': 'Accounting/Accounting',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['account_consolidation'],
+    'auto_install': True,
+    "license": "AGPL-3",
+}
