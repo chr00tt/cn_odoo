990647a8c557812ea8434900f61badc15d5cd800
He Jian <hej@eabax.com>
2024-12-22 23:00:25 +0800

    fix

---
+++ cn_account_budget/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Budget Management',
+    'author': "He Jian",
+    'category': 'Accounting/Accounting',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['account_budget'],
+    'auto_install': True,
+    "license": "AGPL-3",
+}
---
+++ cn_project_account_budget/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': "cn Project Budget",
+    'author': "He Jian",
+    'category': 'Services/Project',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['project_account_budget'],
+    'auto_install': True,
+    'license': 'OEEL-1',
+}
