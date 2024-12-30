17badcdeb86906ce7fb560e48250b97fd60e542b
He Jian <hej@eabax.com>
2024-12-30 22:24:44 +0800

    fix

---
+++ cn_account_analytic_parent/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "cn Account Analytic Parent",
+    'author': "He Jian",
+    "category": "Accounting & Finance",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["account_analytic_parent"],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
