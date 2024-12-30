723ca0688932e869daba9c7658cd984289c0782a
He Jian <hej@eabax.com>
2024-12-30 18:08:34 +0800

    fix

---
+++ cn_auditlog/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2015 ABF OSIELL <https://osiell.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "cn Audit Log",
+    'author': "He Jian",
+    "category": "Tools",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["auditlog"],
+    "auto_install": True,
+    'license': 'LGPL-3',
+}
