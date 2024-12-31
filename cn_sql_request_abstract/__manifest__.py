15d27c3bdfa908702b88588e09226d07bd5d06a1
He Jian <hej@eabax.com>
2024-12-31 08:58:48 +0800

    fix

---
+++ cn_sql_request_abstract/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2017 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "cn SQL Request Abstract",
+    'author': "He Jian",
+    "category": "Tools",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["sql_request_abstract"],
+    "auto_install": True,
+    "license": "AGPL-3",
+}
