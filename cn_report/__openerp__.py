fa94701851957f66f0db8b74d322b99e0b84de59
He Jian <hej@eabax.com>
2025-01-04 10:52:45 +0800

    fix

---
+++ cn_report/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'cn Report',
+    'author': "He Jian",
+    'category': 'Base',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['report'],
+    'data': [
+        'data/cn_report_data.xml',
+    ],
+    'auto_install': True,
+}
