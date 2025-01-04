008530e1620cb83747da3efc8d332f68a003da08
He Jian <hej@eabax.com>
2025-01-04 16:52:55 +0800

    fix

---
+++ cn_stock/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Inventory Management',
+    'description': 'cn Inventory Management',
+    'author': "He Jian",
+    'category': 'Warehouse',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['stock'],
+    'demo': [
+        'data/cn_stock_demo.xml',
+    ],
+    'auto_install': True,
+}
