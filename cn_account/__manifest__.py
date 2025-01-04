11d1c0521a0d5268d749f23fbdf93f8d3d83b356
He Jian <hej@eabax.com>
2025-01-04 17:53:09 +0800

    fix

--- cn_account/__manifest__.py
+++ cn_account/__manifest__.py
@@ -6,12 +6,6 @@
     'author': "He Jian",
     'category': 'Accounting',
     'depends' : ['account'],
-    'data': [
-        'data/cn_account_coa_chart_data.xml',
-        'data/data_account_type.xml',
-        'data/account.account.template.csv',
-        'data/cn_account_coa_post.xml',
-    ],
     'demo': [
         'demo/cn_account_demo.xml',
     ],
