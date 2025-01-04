065056b3d7316666a2c9aae86a147a21b31eb833
He Jian <hej@eabax.com>
2025-01-04 21:55:01 +0800

    fix

---
+++ cn_stock_account/__openerp__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn WMS Accounting',
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['stock_account'],
+    'demo': [
+        'data/cn_stock_account_demo.xml',
+    ],
+    'auto_install': True,
+}
