b226554331ccb321640489bb3381d9cc96db371f
He Jian <hej@eabax.com>
2025-01-06 11:03:04 +0800

    fix

---
+++ cn_product_category_code/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "cn Product Category Code",
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": [
+        "product_category_code",
+    ],
+    'auto_install': True,
+    "license": "AGPL-3",
+}
