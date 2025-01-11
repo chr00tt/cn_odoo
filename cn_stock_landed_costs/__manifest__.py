e6a8fe7f35885872da9e5fe7e5badeba7bfb04cd
He Jian <hej@eabax.com>
2025-01-11 23:57:08 +0800

    fix

---
+++ cn_stock_landed_costs/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn WMS Landed Costs',
+    'author': "He Jian",
+    'description': """
+cn Landed Costs Management
+    """,
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['stock_landed_costs'],
+    'category': 'Warehouse',
+    'sequence': 16,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
---
+++ cn_web/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Web',
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'description':
+        """
+cn Web core module.
+        """,
+    'depends': ['web'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
