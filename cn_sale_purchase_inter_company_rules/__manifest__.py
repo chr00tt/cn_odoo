8ded2cdae3e3432ab3fa850b6cc03985ee084c5d
He Jian <hej@eabax.com>
2025-01-14 14:23:03 +0800

    fix

--- cn_sale_purchase_inter_company_rules/__manifest__.py
+++ cn_sale_purchase_inter_company_rules/__manifest__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-
 {
-    'name': 'cn Sales',
+    'name': 'cn Inter Company Module for Sale/Purchase Orders and Invoices',
     'author': "He Jian",
-    'category': 'Sales/Sales',
+    'category': 'Productivity',
     'website': 'https://github.com/chr00tt/cn_odoo',
+    'description': '',
     'depends': [
-        'sale',
+        'sale_purchase_inter_company_rules',
     ],
     'auto_install': True,
     'license': 'LGPL-3',
