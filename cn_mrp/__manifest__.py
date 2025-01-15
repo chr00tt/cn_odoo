1172415d347307d391bab3f12dcaa62740b9a34f
He Jian <hej@eabax.com>
2025-01-15 11:11:30 +0800

    fix

---
+++ cn_mrp/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'cn Manufacturing',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Manufacturing',
+    'depends': ['mrp'],
+    'description': "",
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
