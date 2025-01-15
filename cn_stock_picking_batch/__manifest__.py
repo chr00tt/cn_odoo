a6f70310f9602217eb9281b7c806494e37a8a082
He Jian <hej@eabax.com>
2025-01-15 20:48:35 +0800

    [I18N] stock_picking_batch.

---
+++ cn_stock_picking_batch/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Warehouse Management: Batch Picking',
+    'author': "He Jian",
+    'category': 'Warehouse',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'description': """
+    """,
+    'depends': ['stock_picking_batch'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
