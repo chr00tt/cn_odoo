2532712273421c9945b8ae5fe14c24f72e250cd6
He Jian <hej@eabax.com>
2025-01-16 16:44:23 +0800

    fix

---
+++ cn_product_manufacturer/__manifest__.py
@@ -0,0 +1,11 @@
+# Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "cn Product Manufacturer",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': "He Jian",
+    "license": "AGPL-3",
+    "category": "Product",
+    "depends": ["product_manufacturer"],
+    "auto_install": True,
+}
