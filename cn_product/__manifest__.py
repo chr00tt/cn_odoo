115b12f95833d1721d584120a1742c55b71fe108
He Jian <hej@eabax.com>
2025-01-18 23:29:23 +0800

    [I18N] product.

---
+++ cn_product/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Products & Pricelists',
+    'author': "He Jian",
+    'category': 'Sales/Sales',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['product'],
+    'description': '',
+    'data': [
+        'data/product_data.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
