db1092edc0ecd5cabb9115bce585f9f33ac0f7e3
He Jian <hej@eabax.com>
2025-01-19 17:33:48 +0800

    [I18N] stock_landed_costs.

---
+++ cn_stock_landed_costs/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn WMS Landed Costs',
+    'author': 'He Jian',
+    'description': '',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['stock_landed_costs'],
+    'category': 'Inventory/Inventory',
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
