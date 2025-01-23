08907f6a451ff1d299cd913341728bbf535754fc
He Jian <hej@eabax.com>
2025-01-23 15:36:12 +0800

    [I18N] barcodes.

---
+++ cn_barcodes/__openerp__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'cn Barcodes',
+    'category': 'Extra Tools',
+    'description': '',
+    'depends': ['barcodes'],
+    'data': [
+        'data/barcodes_data.xml',
+    ],
+    'auto_install': True,
+}
