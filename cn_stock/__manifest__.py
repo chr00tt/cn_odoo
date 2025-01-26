36a666834857b800c1372261c46699489989bcce
He Jian <hej@eabax.com>
2025-01-26 11:16:44 +0800

    [I18N] stock.

---
+++ cn_stock/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Inventory',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['stock'],
+    'category': 'Inventory/Inventory',
+    'demo': [
+        'data/stock_demo_pre.xml',
+    ],
+    'data': [
+        'data/stock_data.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
