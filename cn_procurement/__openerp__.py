bfc535b93ec343ee6677d74f0088a2b82a43ed59
He Jian <hej@eabax.com>
2025-01-27 21:50:01 +0800

    [I18N] procurement.

---
+++ cn_procurement/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name' : 'cn Procurements',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category' : 'Hidden',
+    'depends' : ['procurement'],
+    'description': '',
+    'data': [
+        'procurement_data.xml',
+    ],
+    'auto_install': True,
+}
