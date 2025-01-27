badb46cb025712a7f3a535e1eaed63b24dac1387
He Jian <hej@eabax.com>
2025-01-27 21:21:02 +0800

    fix

---
+++ cn_base_action_rule/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Automated Action Rules',
+    'author': 'He Jian',
+    'category': 'Sales',
+    'description': '',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['base_action_rule'],
+    'auto_install': True,
+}
