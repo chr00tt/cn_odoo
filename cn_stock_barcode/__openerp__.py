5b0fb505753dcf0465cef7df48518a885a60d00a
He Jian <hej@eabax.com>
2025-01-31 21:25:52 +0800

    [I18N] stock_barcode.

---
+++ cn_stock_barcode/__openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "cn Warehouse Management Barcode Scanning",
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'description': '',
+    'category': 'Warehouse',
+    'depends': ['stock_barcode'],
+    'auto_install': True,
+}
