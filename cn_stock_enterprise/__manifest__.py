10a259781ecc143bfa3ff689673e14f95df2815a
He Jian <hej@eabax.com>
2025-02-01 22:04:35 +0800

    fix

--- cn_stock_enterprise/__manifest__.py
+++ cn_stock_enterprise/__manifest__.py
@@ -2,18 +2,12 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'cn Products & Pricelists',
-    'author': "He Jian",
-    'category': 'Sales',
+    'name': "Stock enterprise",
+    'author': 'He Jian',
     'website': 'https://github.com/chr00tt/cn_odoo',
-    'depends': ['product'],
+    'category': "Stock",
     'description': '',
-    'data': [
-        'data/product_data.xml',
-    ],
-    'demo': [
-        'data/product_demo.xml',
-    ],
+    'depends': ['stock_enterprise'],
     'auto_install': True,
     'license': 'LGPL-3',
 }
