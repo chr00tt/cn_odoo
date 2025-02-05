8cc7d88d1ba1ba63e702a4b2846a3f09688804bf
He Jian <hej@eabax.com>
2025-02-05 21:54:43 +0800

    fix

---
+++ cn_stock_barcode_picking_batch/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Barcode for Batch Transfer',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Inventory/Inventory',
+    'description': "",
+    'depends': ['stock_barcode_picking_batch'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
