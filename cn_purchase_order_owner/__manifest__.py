b360431429ef96a7c368d52077278705e9b49f3e
He Jian <hej@eabax.com>
2025-02-07 15:51:35 +0800

    fix

---
+++ cn_purchase_order_owner/__manifest__.py
@@ -0,0 +1,11 @@
+# Copyright 2023 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "cn Purchase Order Owner",
+    'author': 'He Jian',
+    "category": "Purchase Management",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["purchase_order_owner"],
+    "license": "LGPL-3",
+    "auto_install": True,
+}
