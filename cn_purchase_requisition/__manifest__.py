830f7e035c7d7b63080e79ff239f7c4df3989aa7
He Jian <hej@eabax.com>
2025-02-08 16:43:58 +0800

    [I18N] purchase_requisition.

---
+++ cn_purchase_requisition/__manifest__.py
@@ -0,0 +1,10 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'cn Purchase Agreements',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Inventory/Purchase',
+    'description': '',
+    'depends': ['purchase_requisition'],
+    'license': 'LGPL-3',
+}
