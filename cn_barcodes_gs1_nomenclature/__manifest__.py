b95babf98f8f6ef2249cc92f20d9172eb25994c8
He Jian <hej@eabax.com>
2025-02-15 20:40:54 +0800

    fix

---
+++ cn_barcodes_gs1_nomenclature/__manifest__.py
@@ -0,0 +1,14 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Barcode - GS1 Nomenclature',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Hidden',
+    'depends': ['barcodes_gs1_nomenclature'],
+    'data': [
+        'data/barcodes_gs1_rules.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
