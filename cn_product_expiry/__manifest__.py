4ba3c9000c14f293598be456e38f3a794fc8f82b
He Jian <hej@eabax.com>
2025-02-18 22:11:29 +0800

    fix

---
+++ cn_product_expiry/__manifest__.py
@@ -0,0 +1,10 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'cn Products Expiration Date',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Inventory/Inventory',
+    'depends': ['product_expiry'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
