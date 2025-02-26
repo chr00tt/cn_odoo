872a388d210b586a8e317079ffbad4a898b6a0fd
He Jian <hej@eabax.com>
2025-02-26 12:07:11 +0800

    fix

---
+++ cn_purchase_order_type/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2015 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "cn Purchase Order Type",
+    'author': 'He Jian',
+    "license": "LGPL-3",
+    "category": "Purchase Management",
+    "depends": ["purchase_order_type"],
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "auto_install": False,
+}
