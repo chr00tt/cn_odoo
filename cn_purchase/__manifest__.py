0ba2565b95beda6dc49001563f5ed92fa780d07f
He Jian <hej@eabax.com>
2025-03-09 22:58:22 +0800

    fix

---
+++ cn_purchase/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Purchase',
+    'author': "He Jian",
+    'category': 'Purchases',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['purchase'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ cn_purchase_requisition/__manifest__.py
@@ -0,0 +1,9 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'cn Purchase Agreements',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Purchases',
+    'depends' : ['purchase_requisition'],
+    'license': 'LGPL-3',
+}
