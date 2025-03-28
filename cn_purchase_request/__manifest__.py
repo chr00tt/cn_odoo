a983d3c6dfdd679220ee195e2d697e931fbdd32f
He Jian <hej@eabax.com>
2025-03-28 16:59:25 +0800

    fix

---
+++ cn_purchase_request/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2018-2019 ForgeFlow, S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    "name": "cn Purchase Request",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "category": "Purchase Management",
+    "depends": ["purchase_request"],
+    "license": "LGPL-3",
+    "auto_install": True,
+}
