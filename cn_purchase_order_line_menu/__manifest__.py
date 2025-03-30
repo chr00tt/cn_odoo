b5e9cb1669d19903a69cdd635378c738493cb46d
He Jian <hej@eabax.com>
2025-03-30 10:42:04 +0800

    fix

---
+++ cn_purchase_order_line_menu/__manifest__.py
@@ -0,0 +1,10 @@
+
+{
+    "name": "cn Purchase Order Line Menu",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'license': 'LGPL-3',
+    "depends": ["purchase_order_line_menu"],
+    "category": "Inventory/Purchase",
+    "auto_isntall": True,
+}
---
+++ cn_stock_request/__manifest__.py
@@ -0,0 +1,12 @@
+# Copyright 2017-2021 ForgeFlow, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "cn Stock Request",
+    "license": "LGPL-3",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': 'He Jian',
+    "category": "Warehouse Management",
+    "depends": ["stock_request"],
+    "auto_install": True,
+}
