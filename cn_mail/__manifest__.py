b08b1c168a4bd186be7e8a091a6eea0c8646401c
He Jian <hej@eabax.com>
2025-04-07 09:49:23 +0800

    fix

---
+++ cn_mail/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Discuss',
+    'author': "He Jian",
+    'category': 'Discuss',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['mail'],
+    'data': [
+        'data/mail_channel_data.xml',
+    ],
+    'auto_install': True,
+}
