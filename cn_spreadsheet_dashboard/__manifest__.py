afe8648b5adf3ec258b19cd3abbd5c49f1fe99a7
He Jian <hej@eabax.com>
2025-04-13 20:51:32 +0800

    [I18N] spreadsheet.

---
+++ cn_spreadsheet_dashboard/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "cn Spreadsheet dashboard",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "category": "Productivity/Dashboard",
+    "depends": ["spreadsheet_dashboard"],
+    "auto_install": True,
+    "license": "LGPL-3",
+}
