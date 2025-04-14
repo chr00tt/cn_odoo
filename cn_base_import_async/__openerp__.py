ab8c4f94ab4d52b962da209c03b0d91d393e5dea
He Jian <hej@eabax.com>
2025-04-15 01:01:37 +0800

    fix

---
+++ cn_base_import_async/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'cn Asynchronous Import',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'license': 'LGPL-3',
+    'category': 'Generic Modules',
+    'depends': [
+        'base_import_async',
+    ],
+    'auto_install': True,
+}
