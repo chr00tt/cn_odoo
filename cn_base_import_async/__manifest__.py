8a3adcad007bae1d930724ffbfdf0b7835e0ea7f
He Jian <hej@eabax.com>
2025-04-14 11:49:12 +0800

    fix

---
+++ cn_base_import_async/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "cn Asynchronous Import",
+    'author': 'He Jian',
+    'license': 'LGPL-3',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "category": "Generic Modules",
+    "depends": ["base_import_async"],
+    "auto_install": True,
+}
