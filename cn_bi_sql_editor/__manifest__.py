69a8091a14f3c442eca91b070831ea69a3974270
He Jian <hej@eabax.com>
2025-04-17 15:59:41 +0800

    fix

---
+++ cn_bi_sql_editor/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "cn BI SQL Editor",
+    "license": "AGPL-3",
+    "category": "Reporting",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": [
+        "bi_sql_editor",
+    ],
+    "auto_install": True,
+}
