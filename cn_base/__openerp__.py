70535d4a766314a2f144a88b1dd10a76be4847ae
He Jian <hej@eabax.com>
2025-04-20 11:52:13 +0800

    fix

--- cn_base/__openerp__.py
+++ cn_base/__openerp__.py
@@ -7,6 +7,9 @@
     'author': 'He Jian',
     'website': 'https://github.com/chr00tt/cn_odoo',
     'depends': ['base'],
+    'demo': [
+        'base_demo.xml',
+    ],
     'auto_install': True,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
