8acf7fd4bfcac79eb60abdcf163c0ef769c03413
He Jian <hej@eabax.com>
2025-04-23 17:23:13 +0800

    [I18N] quality_control.

---
+++ cn_quality/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'cn Quality Base',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Manufacturing/Quality',
+    'depends': ['quality'],
+    'data': [
+        'data/quality_data.xml',
+    ],
+    'license': 'LGPL-3',
+}
---
+++ cn_quality_control/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'cn Quality',
+    'author': 'He Jian',
+    'category': 'Manufacturing/Quality',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['quality_control'],
+    'data': [
+        'data/quality_control_data.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
