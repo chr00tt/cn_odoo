99d62e1ba07343367482d1802d3a1d18f2bdd39e
He Jian <hej@eabax.com>
2025-04-30 17:42:07 +0800

    fix

---
+++ cn_stock_lot_production_date/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "cn Stock Lot Production Date",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "category": "Warehouse Management",
+    "depends": ["stock_lot_production_date"],
+    "auto_install": True,
+    'license': 'LGPL-3',
+}
