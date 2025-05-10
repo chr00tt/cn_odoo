46ea2f4201e50e348de220541e989b1e9c7a4929
He Jian <hej@eabax.com>
2025-05-10 11:53:56 +0800

    [I18N] barcodes_gs1_nomenclature.

---
+++ cn_barcodes_gs1_nomenclature/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'cn Barcode - GS1 Nomenclature',
+    'author': 'He Jian',
+    'category': 'Accounting/Accounting',
+    'category': 'Hidden',
+    'depends': ['barcodes_gs1_nomenclature'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
