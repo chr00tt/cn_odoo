71cee3fa7ff8034efe940063889a54f58cae33a7
He Jian <hej@eabax.com>
2025-05-11 22:01:18 +0800

    [I18N] purchase_order_type.

---
+++ cn_purchase_order_type/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "cn Purchase Order Type",
+    'author': 'He Jian',
+    'license': 'LGPL-3',
+    "category": "Purchase Management",
+    "depends": ["purchase_order_type"],
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "auto_install": True,
+}
