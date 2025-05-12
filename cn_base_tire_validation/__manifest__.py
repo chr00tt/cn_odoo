bf95ccee0a505115d010a3da9d0b7ba28cda465a
He Jian <hej@eabax.com>
2025-05-12 11:26:40 +0800

    [I18N] base_tire_validation.

---
+++ cn_base_tire_validation/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "cn Base Tier Validation",
+    "category": "Tools",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': 'He Jian',
+    'license': 'LGPL-3',
+    "auto_install": True,
+    "depends": ["base_tire_validation"],
+}
