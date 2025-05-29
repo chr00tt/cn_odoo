e125b0c821d7534002a3c9a205ec7094e544ac77
He Jian <hej@eabax.com>
2025-05-29 10:54:13 +0800

    [I18n] spreadsheet_oca.

---
+++ cn_spreadsheet_oca/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "cn Spreadsheet Oca",
+    "license": "AGPL-3",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["spreadsheet_oca"],
+}
