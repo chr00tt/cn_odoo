5488c4a5ce57795764a56068c3935e482ce8d90b
He Jian <hej@eabax.com>
2025-06-20 09:22:47 +0800

    fix

---
+++ cn_queue_job/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name": "cn Job Queue",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "license": "LGPL-3",
+    "category": "Generic Modules",
+    "depends": ["queue_job"],
+    "auto_install": True,
+}
