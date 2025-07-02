825adcab09f4a1ed264104e930c581605811d5fa
what <18080489073>
2025-07-02 08:53:49 +0800

    [ADD] 密码强度管理

---
+++ cn_password_security/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Password Security",
+    "summary": "Allow admin to set password security requirements.",
+    'author': 'Zhong YingLie',
+    "depends": [
+        'password_security'
+    ],
+    "website": "https://github.com/OCA/server-auth",
+    "license": "LGPL-3",
+    'installable': True,
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
