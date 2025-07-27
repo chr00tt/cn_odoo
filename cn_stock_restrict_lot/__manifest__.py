b9c59ea83412adcf4c37b57eb04822bf8934a4d2
He Jian <hej@eabax.com>
2025-07-27 17:40:13 +0800

    fix

---
+++ cn_stock_restrict_lot/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "cn Stock Restrict Lot",
+    "category": "Warehouse Management",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'author': 'He Jian',
+    "license": "LGPL-3",
+    "depends": ["stock_restrict_lot"],
+    'auto_install': True,
+}
