c1b654b9c153360bf766eba22662715a5153fe28
He Jian <hej@eabax.com>
2025-08-05 18:18:43 +0800

    [I18N] hr_department_code.

---
+++ cn_hr_department_code/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "cn HR department code",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["hr_department_code"],
+    "auto_install": True,
+}
