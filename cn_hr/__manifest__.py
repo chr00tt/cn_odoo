b072627b982b06ddae522bb6ee93007f081d2395
He Jian <hej@eabax.com>
2025-08-11 11:50:36 +0800

    fix

---
+++ cn_hr/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Employees',
+    'author': 'He Jian',
+    'category': 'Human Resources/Employees',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': [
+        'hr',
+    ],
+    'data': [
+        'data/hr_data.xml',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
