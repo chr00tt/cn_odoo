6a53e7b5e05521f426101774965b3034a3d18ac2
He Jian <hej@eabax.com>
2025-08-12 10:38:58 +0800

    fix

--- cn_resource/__manifest__.py
+++ cn_resource/__manifest__.py
@@ -6,6 +6,9 @@
     'category': 'Hidden',
     'website': 'https://github.com/chr00tt/cn_odoo',
     'depends': ['resource'],
+    'data': [
+        'data/resource_data.xml',
+    ],
     'auto_install': True,
     'license': 'LGPL-3',
 }
