727bfdae7c952e0c298cbe4832600f3eb86582d7
He Jian <hej@eabax.com>
2025-08-22 16:39:26 +0800

    fix

---
+++ cn_web_enterprise/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Web Enterprise',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Hidden',
+    'depends': ['web_enterprise'],
+    'auto_install': True,
+    'license': 'OEEL-1',
+}
