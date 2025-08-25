104122b257a198916e065f971c8dac809b2db924
Liyh <380568933@qq.com>
2025-08-25 16:27:14 +0800

    第一次提交分支

---
+++ cn_hr_expense_advance_clearing/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Kitti Upariphutthiphong <kittiu@ecosoft.co.th>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "cn Employee Advance and Clearing",
+    'author': "He Jian",
+    "category": "Human Resources",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    "depends": ["hr_expense_advance_clearing"],
+    "demo": [
+        'demo/product_template_demo.xml',
+    ],
+    'auto_install': True,
+    "license": "AGPL-3",
+}
