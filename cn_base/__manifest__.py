0dac9907a7f4dae27b7d42493860335954344fc8
He Jian <hej@eabax.com>
2025-10-30 10:07:14 +0800

    Fix

--- cn_base/__manifest__.py
+++ cn_base/__manifest__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 
 {
     'name': 'cn Base',
@@ -8,6 +6,9 @@
     'website': 'https://github.com/chr00tt/cn_odoo',
     'category': 'Hidden',
     'depends': ['base'],
+    'data': [
+        'data/cn_base_data.xml',
+    ],
     'auto_install': True,
     'license': 'LGPL-3',
 }
---
+++ cn_base_import/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'cn Base import',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['base_import'],
+    'category': 'Hidden/Tools',
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
