a4bde794ddd79251bd59de4b9c0a74eb3d3aaee6
He Jian <hej@eabax.com>
2025-12-18 15:31:22 +0800

    [I18N] account

---
+++ cn_account/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'cn Invoicing',
+    'category': 'Accounting/Accounting',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['account'],
+    'auto_install': True,
+    'author': 'He Jian',
+    'license': 'LGPL-3',
+}
