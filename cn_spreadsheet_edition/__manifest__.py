6d0b093c18ee8e02429445b537627a3a43ceb2d8
He Jian <hej@eabax.com>
2026-01-23 10:11:16 +0800

    Fix

---
+++ cn_spreadsheet_edition/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cn Spreadsheet",
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Hidden',
+    'depends': ['spreadsheet_edition'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
