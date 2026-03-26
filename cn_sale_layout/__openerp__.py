85a1f6387381feb0f19b729bac1a3dd41af7c7d5
He Jian <hej@eabax.com>
2026-03-26 11:40:37 +0800

    [I18N] sale_layout.

---
+++ cn_sale_layout/__openerp__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Sale Layout',
+    'author': 'He Jian',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['sale_layout'],
+    'category': 'Sale',
+    'demo': ['data/sale_layout_category_data.xml'],
+    'auto_install': True,
+}
