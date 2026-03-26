3889ce953dff17589ef11fd701abf139bf27b7e1
He Jian <hej@eabax.com>
2026-03-26 22:48:58 +0800

    [I18N] sale.

---
+++ cn_sale/__openerp__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Sales Management',
+    'author': "He Jian",
+    'category': 'Sales',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['sale'],
+    'auto_install': True,
+}
