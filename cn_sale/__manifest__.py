2eeaaa149091f778a9c171c3f01136d7848b8eef
He Jian <hej@eabax.com>
2026-03-27 23:21:14 +0800

    [I18N] sale.

---
+++ cn_sale/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Sales',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Sales',
+    'depends': ['sale'],
+    'auto_install': True,
+}
---
+++ cn_sale_timesheet/__manifest__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Sales Timesheet',
+    'author': "He Jian",
+    'category': 'Hidden',
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'depends': ['sale_timesheet'],
+    'auto_install': True,
+}
