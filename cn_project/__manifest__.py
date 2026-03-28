57fc75a254ab07e1830576841b78440e7c8de060
He Jian <hej@eabax.com>
2026-03-28 22:12:53 +0800

    [I18N] project,sale.

---
+++ cn_project/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Project',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Project',
+    'depends': [
+        'project',
+    ],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
---
+++ cn_sale/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'cn Sales',
+    'author': "He Jian",
+    'website': 'https://github.com/chr00tt/cn_odoo',
+    'category': 'Sales',
+    'depends': ['sale'],
+    'auto_install': True,
+    'license': 'LGPL-3',
+}
